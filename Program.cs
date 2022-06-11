79a6828a584d58085e38b883f6043aa7dc64cc2f
pcrep <pcrep@DESKTOP-UPBAUEP>
2022-06-11 17:55:47 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+float bottom;
+float height;
+float area;
+
+Console.WriteLine("Welcome! I will find the area of a triangle for you\n");
+Console.WriteLine("What is the base?");
+bottom = float.Parse(Console.ReadLine());
+Console.WriteLine("What is the height?");
+height = float.Parse(Console.ReadLine());
+
+area = (bottom * height) / 2;
+
+
+Console.WriteLine("The bottom is\n" + bottom);
+Console.WriteLine("the height is\n" + height);
+Console.WriteLine("The area is " + area);
+
+
+
